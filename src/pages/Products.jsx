04ac39e1acd31782fa6c1c0813b5d9f6a7c74bdd
Muhammadali06dev <muhammadalideveloper2006@gmail.com>
2023-09-22 05:42:24 +0500

    add filters page

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,22 +1,28 @@
-import Filter from "../components/Filters"
+import Filters from "../components/Filters"
 import { ProductsContainer, PaginationContainer } from "../components"
 import { customFetch } from "../utils"
 
 const url = "/products"
 export const loader = async ({ request }) => {
-   const response = await customFetch(url)
-   const products = response.data.data
-   const meta = response.data.meta
-   return { products, meta }
-}
+   console.log(request)
+   const params = Object.fromEntries([
+      ...new URL(request.url).searchParams.entries()
+   ])
+   const response = await customFetch(url, {
+      params
+   });
+   const products = response.data.data;
+   const meta = response.data.meta;
+   return { products, params, meta };
+};
 
 function Products() {
    return (
-      <div>
-         <Filter />
+      <section className="py-20">
+         <Filters />
          <ProductsContainer />
          <PaginationContainer />
-      </div>
+      </section>
    )
 }
 
