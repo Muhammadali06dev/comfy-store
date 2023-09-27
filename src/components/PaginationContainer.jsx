a8a5a28b558f6c17abc3ef002e6f3a05bb37f7d6
Muhammadali06dev <muhammadalideveloper2006@gmail.com>
2023-09-27 09:02:16 +0500

    fix bugs on pagination

--- src/components/PaginationContainer.jsx
+++ src/components/PaginationContainer.jsx
@@ -24,7 +24,7 @@ function PaginationContainer() {
       <div className="mt-16 flex justify-end ">
          <div className="join">
             <button
-               className="btn btn-sx sm:btn-md join-item"
+               className="btn btn-xs sm:btn-md join-item"
                onClick={() => {
                   let prevPage = page - 1
                   if (prevPage < 1) prevPage = pageCount
@@ -45,7 +45,7 @@ function PaginationContainer() {
                )
             })}
             <button
-               className="btn btn-sx sm:btn-md join-item"
+               className="btn btn-xs sm:btn-md join-item"
                onClick={() => {
                   let nextPage = page + 1
                   if (nextPage > pageCount) nextPage = 1
